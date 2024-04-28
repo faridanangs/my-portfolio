c8552513164de6252fff3b0f6dd901f90c96042c
faridanang <f4r1danangs@gmail.com>
2024-04-28 18:25:43 +0700

    update project

--- src/components/HerroSection.js
+++ src/components/HerroSection.js
@@ -11,11 +11,13 @@ const HerroSection = () => {
         <div className='flex items-center justify-between min-h-[60vh] md:min-h-[60vh] flex-col-reverse md:flex-row'>
             <div className='space-y-10 text-center md:text-start'>
                 <h1 className='text-3xl lg:text-6xl md:text-4xl font-bold mt-4'>Nice to meet you! 👋<br />
-                    <span className='underline underline-offset-7 decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm FaridAnang.</span>
+                    <span className='underline underline-offset-7 decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm Faridanang.</span>
                 </h1>
                 <p className='md:pr-5 lg:max-w-xl max-w-lg text-lg text-gray-400'>
                     Hello, my name is Farid Anang Samudra,
-                    I am a junior <span className='text-xl font-bold text-white/80'>full-stack web developer</span> and <span className='text-xl font-bold text-white/80'>blockchain developer</span>.
+                    I am a junior <span className='text-xl font-bold text-white/80'>full-stack web developer</span> and <span className='text-xl font-bold text-white/80'>blockchain developer</span>, I can
+                    assist you in creating the systems you need to enhance your company's or
+                    business's profitability.
                 </p>
                 <Link href="faridanangs11@gmail.com" className='inline-block '>
                     <div>
@@ -32,7 +34,7 @@ const HerroSection = () => {
                 </Link>
             </div>
             <div className='relative'>
-                <Image src={myImage} alt='my image' height={250} width={250} priority quality={100} className='object-cover opacity-50 rounded-md lg:w-[20rem]' />
+                <Image src={myImage} alt='my image' height={250} width={250} priority quality={100} className='object-cover opacity-70 rounded-md lg:w-[20rem]' />
                 <div className="absolute bottom-5 md:bottom-4 left-0">
                     <MovingBorderBtn borderRadius='0.5rem' className="p-2 md:p-3 font-semibold">
                         <Link href="https://drive.google.com/file/d/1M-up5O23twAOqDMim-GAKCHB87POXIcr/view?usp=drive_link"><span className='text-xl'>💼</span> Available for Work(CV)</Link>
