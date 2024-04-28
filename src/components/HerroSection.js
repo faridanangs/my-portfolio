64153283dd03e7d6c9ea9fe0f859d654cdda36e4
faridanang <f4r1danangs@gmail.com>
2024-04-28 18:23:31 +0700

    update project

--- src/components/HerroSection.js
+++ src/components/HerroSection.js
@@ -11,13 +11,11 @@ const HerroSection = () => {
         <div className='flex items-center justify-between min-h-[60vh] md:min-h-[60vh] flex-col-reverse md:flex-row'>
             <div className='space-y-10 text-center md:text-start'>
                 <h1 className='text-3xl lg:text-6xl md:text-4xl font-bold mt-4'>Nice to meet you! 👋<br />
-                    <span className='underline underline-offset-7 decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm Faridanang.</span>
+                    <span className='underline underline-offset-7 decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm FaridAnang.</span>
                 </h1>
                 <p className='md:pr-5 lg:max-w-xl max-w-lg text-lg text-gray-400'>
-                    Hello, my name is farid anang samudra,
-                    I am a junior <span className='text-xl font-bold text-white/80'>full-stack web developer</span> and <span className='text-xl font-bold text-white/80'>blockchain developer</span>, I can
-                    assist you in creating the systems you need to enhance your company's or
-                    business's profitability.
+                    Hello, my name is Farid Anang Samudra,
+                    I am a junior <span className='text-xl font-bold text-white/80'>full-stack web developer</span> and <span className='text-xl font-bold text-white/80'>blockchain developer</span>.
                 </p>
                 <Link href="faridanangs11@gmail.com" className='inline-block '>
                     <div>
