4d7f08e5f8b0ca1a3b22caa5a0c20ab53044b51f
faridanang <f4r1danangs@gmail.com>
2024-04-30 20:23:00 +0800

    commit

--- src/components/HerroSection.js
+++ src/components/HerroSection.js
@@ -10,8 +10,8 @@ const HerroSection = () => {
     return (
         <div className='flex items-center justify-between min-h-[60vh] md:min-h-[60vh] flex-col-reverse md:flex-row animate-move-up'>
             <div className='space-y-10 text-center md:text-start'>
-                <h1 className='text-3xl lg:text-6xl md:text-4xl font-bold mt-4'>Nice to meet you! <span className='hand'>👋</span><br />
-                    <span className='underline underline-offset-7 decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm Faridanang.</span>
+                <h1 className='text-3xl lg:text-6xl md:text-4xl font-bold mt-4'>Nice to meet you, <span className='hand'>👋</span><br />
+                    <span className='underline underline-offset-[3px] md:underline-offset-[4px] lg:underline-offset-[6px] decoration-purple-300 bg-clip-text text-transparent bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 '>I'm Faridanang</span>
                 </h1>
                 <p className='md:pr-5 lg:max-w-xl max-w-lg text-lg text-gray-400 px-1 md:px-0'>
 
@@ -37,9 +37,9 @@ const HerroSection = () => {
             </div>
             <div className='relative'>
                 <Image src={myImage} alt='my image' height={250} width={250} priority quality={100} className='object-cover opacity-70 rounded-md lg:w-[20rem]' />
-                <div className="absolute bottom-5 md:bottom-4 left-0">
-                    <MovingBorderBtn borderRadius='0.5rem' className="p-2 md:p-3 font-semibold">
-                        <Link href="https://drive.google.com/file/d/1uFnSZz1dlZtTEeH14phamZXbcrfrcXUC/view?usp=sharing"><span className='text-xl'>💼</span> Available for Work(CV)</Link>
+                <div className="absolute bottom-3 -left-12 md:bottom-4 md:left-0">
+                    <MovingBorderBtn borderRadius='0.5rem' className="p-1 px-2 md:p-1 md:px-2 font-semibold">
+                        <Link href="https://drive.google.com/file/d/1uFnSZz1dlZtTEeH14phamZXbcrfrcXUC/view?usp=sharing"><span className='text-xl'>💼</span> Available for Work ( CV )</Link>
                     </MovingBorderBtn>
                 </div>
             </div>
